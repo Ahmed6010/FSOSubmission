5fd4b7fb5d535d2abc18e5215302d8d2d9163671
Ahmed6010 <ahmed15151@hotmail.fr>
2020-12-14 19:09:52 +0100

    Part 3

--- part 3/phonebook/index.js
+++ part 3/phonebook/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
-
+app.use(cors())
 morgan.token('logPostReq', (req, res) => JSON.stringify(req.body) );
-
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :logPostReq'))
 
