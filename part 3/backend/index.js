5bf468111fe0bd65bc887fc62339a77f04a5bb05
Ahmed6010 <ahmed15151@hotmail.fr>
2020-12-15 22:05:34 +0100

    Part 3

--- part 3/backend/index.js
+++ part 3/backend/index.js
@@ -4,6 +4,7 @@ var morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 morgan.token('logPostReq', (req, res) => JSON.stringify(req.body) );
 
