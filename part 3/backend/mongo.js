49ebd77fd5efcbbb9e449a2bbd876198b2b551ec
Ahmed6010 <ahmed15151@hotmail.fr>
2020-12-16 23:09:40 +0100

    Part 3

---
+++ part 3/backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.ew0hp.mongodb.net/person-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+if(process.argv.length === 3){
+    console.log('phonebook :');
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}else{
+    person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+    }).catch(err => {
+        console.log(err);
+        mongoose.connection.close()    
+    })
+}
